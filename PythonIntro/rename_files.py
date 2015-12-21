b4404fae66359d86a7ceda06cd2dd6c855f58c87
kohta21 <kohta.okamoto0704@gmail.com>
2015-12-22 00:02:12 +0900

    s

--- PythonIntro/rename_files.py
+++ PythonIntro/rename_files.py
@@ -5,7 +5,7 @@ import os
 
 def rename_files():
     #(1) get file names from a folder
-    file_list = s.listdir(r".\")
+    file_list = os.listdir(r"C:\Users\kohta\Documents\Visual Studio 2015\Projects\PythonIntro\PythonIntro\prank")
     print (file_list)
     
 
